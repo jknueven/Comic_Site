d6157821c21e768bbd10348163bb068c1dcb1d79
Jared <jcknueven@gmail.com>
2016-06-17 15:59:40 -0400

    added chara page with js

--- js/chara.js
+++ js/chara.js
@@ -1,9 +1,27 @@
 $( document ).ready(function() {
+	// Handler for .ready() called.
 
-$.ajax({url: "http://gateway.marvel.com:80/v1/public/characters?apikey=a3d450fd87cce2aeff11efbcc111f789", success: function(result)
-	{
-
-	}
+$("body").on("click", '.clicker',function(e){
+  var content = $(this).find('.dropdown').toggle();
 });
+
+$.ajax({url: "http://gateway.marvel.com:80/v1/public/characters?apikey=a3d450fd87cce2aeff11efbcc111f789", success: function(result){
+
+	function setImgTable(){
+		var characters = result.data.results;
+
+		characters.forEach(function(character){
+			var charaThumbnail = character.thumbnail.path+"."+character.thumbnail.extension;
+		$(".charaContent").append("<div class='col-md-4 clicker'><img src='"+charaThumbnail+"'><div class='dropdown'></div></div</div>");
+		})
+		
+	}
+
+	setImgTable();
+	setInterval(setImage, 5000 );
+
+
+}});
+
 
 });
--- js/main.js
+++ js/main.js
@@ -28,6 +28,34 @@ $.ajax({url: "http://gateway.marvel.com:80/v1/public/characters?apikey=a3d450fd8
 	}
 });
 
+$.ajax({url: "http://gateway.marvel.com:80/v1/public/comics?apikey=a3d450fd87cce2aeff11efbcc111f789", success: function(result)
+	{
+		console.log(result)
+
+		function randomGenerator(e) {
+			return Math.floor(Math.random() * e) + 1;
+		}
+
+
+		function setImage(){
+			var comics = result.data.results;
+			var random = randomGenerator(comics.length);
+
+			var comic = comics[random];
+			console.log(comic);
+			var comicThumbnail = comic.images[0].path+"."+comic.images[0].extension;
+
+			$(".comicIMGone").html("<img src='"+comicThumbnail+"'>");
+		}
+
+		setImage();
+		setInterval(setImage, 5000 );
+
+
+		// console.log(character);
+	}
+});
+
 
 
 });
